b016f40dbe210115254667b3f8e2a83887821a13
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-15 14:02:50 +0600

    Contact Page Added & changes in admin master

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^logout/$', views.mylogout, name='mylogout'),
     url(r'^panel/setting/$', views.site_setting, name='site_setting'),
     url(r'^panel/about/setting/$', views.about_setting, name='about_setting'),  ## This is for about(about seeting) in admin panel
+    url(r'^contact/$', views.contact, name='contact'), 
 ]

402847781585f5a1513f4c130165d9759503b252
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-23 19:37:16 +0600

    Delete and Edit Trending

--- trending/urls.py
+++ trending/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
 
-    url(r'^panel/trending/$', views.trending_add, name='trending_add'),   ## Admin Panel Trending Add    
+    url(r'^panel/trending/$', views.trending_add, name='trending_add'),   ## Admin Panel Trending Add   
+    url(r'^panel/trending/del/(?P<pk>\d+)/$', views.trending_del, name='trending_del'),   ## Admin Panel Delete Trending 
+    url(r'^panel/trending/edit/(?P<pk>\d+)/$', views.trending_edit, name='trending_edit'),   ## Admin Panel Edit Trending 
             
 ]  
--- trending/views.py
+++ trending/views.py
@@ -33,3 +33,40 @@ def trending_add(request):
 
     return render(request, 'back/trending.html', {'trendinglist':trendinglist})
 ###---#---### Trending Add Function For Back (Admin Panel - Backend) End ####--#--####
+
+
+###---#---### Trending Delete Function For Back (Admin Panel - Backend) Start ####--#--####
+def trending_del(request, pk):
+
+    # Login check Start
+    if not request.user.is_authenticated:
+        return redirect('mylogin')
+    # Login check End
+
+    b = Trending.objects.filter(pk=pk)
+    b.delete()
+
+    return redirect('trending_add')
+###---#---### Trending Delete Function For Back (Admin Panel - Backend) End ####--#--####
+
+
+###---#---### Trending Edit Function For Back (Admin Panel - Backend) Start ####--#--####
+def trending_edit(request, pk):
+
+    mytxt = Trending.objects.get(pk=pk).txt
+
+    if request.method == 'POST':
+
+        txt = request.POST.get('txt')
+
+        if txt == "" :
+            error = "All Fields Required"
+            return render(request, 'back/error.html', {'error':error})
+
+        b = Trending.objects.get(pk=pk)
+        b.txt = txt
+        b.save()
+        return redirect('trending_add')
+
+    return render(request, 'back/trending_edit.html', {'mytxt':mytxt, 'pk':pk})
+###---#---### Trending Edit Function For Back (Admin Panel - Backend) End ####--#--####

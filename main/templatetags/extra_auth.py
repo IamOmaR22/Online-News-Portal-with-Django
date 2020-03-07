76f121352eff82c68ddcfd71af4f3c2011316e0a
Md. Omar Faruk <iamomar022@gmail.com>
2020-03-07 23:24:40 +0600

    Check Users's Group In Template

--- main/templatetags/extra_auth.py
+++ main/templatetags/extra_auth.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import Group
 
 register = template.Library()
 
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()

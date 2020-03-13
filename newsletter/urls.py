e7636760124dc6f0273667a3fb91e11e146d6b0c
Md. Omar Faruk <iamomar022@gmail.com>
2020-03-13 12:31:39 +0600

    Newsletter Done

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,6 +3,9 @@ from .import views
 
 urlpatterns = [
 
-    
+    url(r'^newsletter/add/$', views.news_letter, name='news_letter'),
+    url(r'^panel/newsletter/emails/$', views.news_emails, name='news_emails'),
+    url(r'^panel/newsletter/phones/$', views.news_phones, name='news_phones'),
+    url(r'^panel/newsletter/del/(?P<pk>\d+)/(?P<num>\d+)/$', views.news_txt_del, name='news_txt_del'),
 
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -12,3 +12,72 @@ from django.contrib.auth.models import User, Group, Permission ## For User Group
 from django.contrib.contenttypes.models import ContentType  ## To add Permissions
 
 # Create your views here.
+
+def news_letter(request):
+
+    if request.method == 'POST':
+
+        txt = request.POST.get('txt')
+
+        res = txt.find('@')
+        
+        if int(res) != -1 :
+            b = Newsletter(txt=txt, status=1)
+            b.save()
+
+        else:
+            try:
+                int(txt)
+                b = Newsletter(txt=txt, status=2)
+                b.save()
+
+            except:
+                return redirect('home')
+
+    return redirect('home')
+
+
+##--#--## Newsletter (Emails) Function For Back (Admin Panel - Backend) Start ##--#--##
+def news_emails(request):
+
+    # Login check Start
+    if not request.user.is_authenticated:
+        return redirect('mylogin')   # when user is not logged in, it will take you the login page(mylogin)
+    # Login check End
+
+    emails = Newsletter.objects.filter(status=1)
+
+    return render(request, 'back/emails.html', {'emails':emails})
+##--#--## Newsletter (Emails) Function For Back (Admin Panel - Backend) End ##--#--##
+
+
+##--#--## Newsletter (Phones) Function For Back (Admin Panel - Backend) Start ##--#--##
+def news_phones(request):
+
+    # Login check Start
+    if not request.user.is_authenticated:
+        return redirect('mylogin')   # when user is not logged in, it will take you the login page(mylogin)
+    # Login check End
+
+    phones = Newsletter.objects.filter(status=2)
+
+    return render(request, 'back/phones.html', {'phones':phones})
+##--#--## Newsletter (Phones) Function For Back (Admin Panel - Backend) End ##--#--##
+
+
+##--#--## Delete Newsletter (Emails and Phones) Function For Back (Admin Panel - Backend) Start ##--#--##
+def news_txt_del(request, pk, num):
+
+    # Login check Start
+    if not request.user.is_authenticated:
+        return redirect('mylogin')   # when user is not logged in, it will take you the login page(mylogin)
+    # Login check End
+
+    b = Newsletter.objects.get(pk=pk)
+    b.delete()
+
+    if int(num) == 2 :
+        return redirect('news_phones')
+
+    return redirect('news_emails')
+##--#--## Delete Newsletter (Emails and Phones) Function For Back (Admin Panel - Backend) End ##--#--##

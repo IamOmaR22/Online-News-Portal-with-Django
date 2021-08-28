556c4f4e50f7768ad01377da4cc110cf3f049675
Jayakrishnan Jayu <jayu2442@gmail.com>
2021-08-28 23:39:55 +0530

    set debug False

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '08nva+ja-kswomarqd*i5bcx+@e&)$uo6nsxmmfarukp04^k_ackz&0r*8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'glacial-woodland-71457.herokuapp.com',
